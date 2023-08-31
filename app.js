11bd13db145a5a681845d9401d2bb79bb5e514f7
ayong <007ayongsvip@gmail.com>
2023-08-31 17:05:04 +0800

    增加分片下载及断点续传

--- app.js
+++ app.js
@@ -2,96 +2,138 @@ import http from 'http';
 import got from 'got';
 import url from 'url';
 import path from 'path';
-// import HttpsProxyAgent from 'https-proxy-agent';
-
 
 const server = http.createServer(async (req, res) => {
-  const parsedUrl = url.parse(req.url, true);
-  
-  const query = parsedUrl.query;
-  console.log(query);
-  let downloadUrl = query.url;
-  const time = query.time;
-  // const token = query.token;
+    const rangeHeader = req.headers['range'];
+    const parsedUrl = url.parse(req.url, true);
+    const query = parsedUrl.query;
+    const decodedUrl = decodeURIComponent(query.url);
+    const time = query.time;
 
 
-  // 检查参数是否为空
-  if (!downloadUrl || !time) {
-    res.statusCode = 401;
-    res.end("Missing required parameters.");
-    return;
-  }
-
-  // 时间戳的简单验证
-  const now = new Date().getTime();
-  const timeDiff = now - time;
-  const threeHour = 1000 * 60 * 60 * 3;
-  if (timeDiff < 0 || timeDiff > threeHour) {
-    res.statusCode = 401;
-    res.end("Token expired, please request a new one.");
-    return;
-  }
-
-
-
-  let fileName = path.basename(parsedUrl.query.url);
-
-
-  try {
-
-    const preResponse = await got.head(downloadUrl, {
-      timeout: { socket: 60000 },
-      followRedirect: false,
-      // agent: {
-      //   http: new HttpsProxyAgent('http://127.0.0.1:7890'),
-      //   https: new HttpsProxyAgent('http://127.0.0.1:7890'),
-      // }
-    });
-
-    if (preResponse.statusCode >= 300 && preResponse.statusCode < 400 && preResponse.headers.location) {
-      downloadUrl = preResponse.headers.location;
-      fileName = path.basename(downloadUrl);
+    // 检查参数是否为空
+    if (!decodedUrl || !time) {
+        res.statusCode = 401;
+        res.end("Missing required parameters.");
+        return;
     }
 
-
-    const response = got.stream(downloadUrl, {
-      timeout: { socket: 60000 },
-      // headers: {
-
-      // },
-      // agent: {
-      //   http: new HttpsProxyAgent('http://127.0.0.1:7890'),
-      //   https: new HttpsProxyAgent('http://127.0.0.1:7890'),
-      // }
-    });
-    response.on('response', (response) => {
-      res.setHeader('Access-Control-Allow-Origin', '*');
-      res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
-      res.setHeader('Access-Control-Max-Age', 60 * 60 * 24 * 30);
-      res.setHeader('Content-Type', response.headers['content-type']);
-      res.setHeader('Content-Length', response.headers['content-length']);
-      console.log(fileName);
-      res.setHeader('Content-Disposition', 'attachment; filename=' + fileName);
-      res.setHeader('Access-Control-Expose-Headers', 'Content-Disposition');
-    })
-    response.on('error', (error) => {
-      console.error(error);
-      res.statusCode = 404;
-      res.end(error.message);
-    });
-    response.pipe(res);
-  } catch (error) {
-    if (error.name === "TimeoutError") {
-      console.error(error);
-      res.statusCode = 504;
-      res.end("请求超时");
-    } else {
-      console.error(error);
-      res.statusCode = 404;
-      res.end("请求失败");
+    // 时间戳的简单验证
+    const now = new Date().getTime();
+    const timeDiff = now - time;
+    const threeHour = 1000 * 60 * 60 * 3;
+    if (timeDiff < 0 || timeDiff > threeHour) {
+        res.statusCode = 401;
+        res.end("Token expired, please request a new one.");
+        return;
     }
-  }
 
+    try {
+        const preResponse = await got.head(decodedUrl, {
+            timeout: { socket: 60000 },
+            followRedirect: false,
+        });
+
+        if (preResponse.statusCode >= 300 && preResponse.statusCode < 400 && preResponse.headers.location) {
+            const redirectUrl = preResponse.headers.location;
+            // console.log('Redirect URL:', redirectUrl);
+
+            try {
+                const redirectResponse = await got.head(redirectUrl, {
+                    timeout: { socket: 60000 },
+                    followRedirect: false,
+                });
+
+                if (redirectResponse.statusCode === 200) {
+                    let fileName = path.basename(redirectUrl);
+                    const contentDisposition = redirectResponse.headers['content-disposition'];
+
+                    if (contentDisposition) {
+                        const match = /filename="(.*?)"/.exec(contentDisposition);
+                        if (match && match[1]) {
+                            fileName = match[1];
+                        }
+                    }
+                    const fileSize = parseInt(redirectResponse.headers['content-length']);
+                    if (!isNaN(fileSize) && fileSize > 0) {
+                        // Set appropriate response headers for content disposition, content type, etc.
+                        // ...
+
+                        res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+                        res.setHeader('Content-Length', fileSize);
+
+                        const response = got.stream(redirectUrl, {
+                            timeout: { socket: 60000 },
+                            headers: {
+                                Range: rangeHeader || `bytes=0-${fileSize - 1}`,
+                            },
+                        });
+
+                        response.on('error', (error) => {
+                            console.error(error);
+                            res.statusCode = 404;
+                            res.end(error.message);
+                        });
+
+                        response.pipe(res);
+                        return;
+                    }
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        } else if (preResponse.statusCode === 200) {
+            let fileName = path.basename(decodedUrl);
+            const contentDisposition = preResponse.headers['content-disposition'];
+
+            if (contentDisposition) {
+                const match = /filename="(.*?)"/.exec(contentDisposition);
+                if (match && match[1]) {
+                    fileName = match[1];
+
+                }
+            }
+            const fileSize = parseInt(preResponse.headers['content-length']);
+            if (!isNaN(fileSize) && fileSize > 0) {
+                // Check if the filename has an extension like .zip, .exe, etc.
+                if (path.extname(fileName)) {
+                    // Set appropriate response headers for content disposition, content type, etc.
+                    // ...
+
+                    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+                    res.setHeader('Content-Length', fileSize);
+
+                    const response = got.stream(decodedUrl, {
+                        timeout: { socket: 60000 },
+                        headers: {
+                            Range: rangeHeader || `bytes=0-${fileSize - 1}`,
+                        },
+                    });
+
+                    response.on('error', (error) => {
+                        console.error(error);
+                        res.statusCode = 404;
+                        res.end(error.message);
+                    });
+
+                    response.pipe(res);
+                    return;
+                }
+            }
+
+
+        }
+    } catch (error) {
+        if (error.name === "TimeoutError") {
+            console.error(error);
+            res.statusCode = 504;
+            res.end("请求超时");
+        } else {
+            console.error(error);
+            res.statusCode = 404;
+            res.end("请求失败");
+        }
+    }
 });
 
 server.listen(3000);
